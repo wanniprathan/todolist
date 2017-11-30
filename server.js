43023ae6f6797b94e3a8fe0fcc02cf1eea1f6ead
Wanwisa Prathan <wanwip15@uia.no>
2017-11-30 11:04:10 +0100

    vær så snill og funk da

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use('/listitem/', listitem);
 //var str = __dirname.substring(0, __dirname.length - 6);
 
 
-/*app.get('/CreateUser',function(req,res){
+app.get('/CreateUser',function(req,res){
     
     //console.log(str);
      //res.sendFile(path.join(str + '/client/CreateUser.html'));
